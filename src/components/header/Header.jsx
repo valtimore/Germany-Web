7172e8b94ee742f59fd2ee075a20d43e1c5376d4
Valti <valentinalondono911@gmail.com>
2024-11-14 23:41:01 -0500

    Changes in principal page
    - create a new logo
    - change components

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,12 +4,12 @@ import './header.css'
 export default function Header() {
     return(
         <header className='header'>
-            <img className='header-img' src='../../../public/icons/logo-completo.png' alt="Plane Icon" /> 
+            <img className='header-img' src='https://firebasestorage.googleapis.com/v0/b/gym-stats-e3823.appspot.com/o/univalleGermany%2Flogo-completo.png?alt=media&token=f238e565-41d7-4d01-930b-eb2e6a46eae6' alt="Plane Icon" /> 
             <nav className='header-nav'>
                 <ul className='header-ul'>
                     <il className='header-li'><a href='#' className='header-a'>Principal</a></il>
                     <il className='header-li'><a href='#' className='header-a'>About us</a></il>
-                    <il className='header-li'><a href='#' className='header-a'>Our plan</a></il>
+                    <il className='header-li'><a href='#' className='header-a'>Activities</a></il>
                     <il className='header-li'><a href='#' className='header-a'>Itinerary</a></il>
                 </ul>
                 <p className='header-p'>On a journey to make a dream come true</p>
--- src/pages/PrincipalPage.jsx
+++ src/pages/PrincipalPage.jsx
@@ -7,7 +7,36 @@ export default function PrincipalPage() {
             <Header />
             <section className='mainContent-bodySection'>
                 <section className='mainContent-bodySection-cover'>
-                    <h1 className='mainContent-bodySection-cover-h1'>Univallunos en Alemania</h1>
+                    <div className='mainContent-bodySection-cover-text'>
+                        <h1>Univalle in Germany</h1>
+                        <p>We are a group of dedicated students from Colombia, representing Universidad del Valle, 
+                            Tuluá branch, currently in our seventh and eighth semesters of Systems Engineering. 
+                            Our goal is to travel to Germany to expand our knowledge in Machine Learning.</p>
+                    </div>
+                </section>
+                <section className='mainContent-aboutUs'>
+                        <h2>About Us</h2>
+                        <div className='test'>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                        </div>
+                        <p> La idea es incluir una breve descripción de cada uno, junto con
+                                cualidades que consideremos importantes.
+                        </p>
+                </section>
+                <section className='mainContent-aboutUs'>
+                        <h2>Activities</h2>
+                        <div className='test'>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                        </div>
+                        <p> Aquí habrá una galería con las fotos de cada actividad que se realice
+                            ya sea con fines de generar fondos o cosas culturales.
+                        </p>
                 </section>
             </section>
             
