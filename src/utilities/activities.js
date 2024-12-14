d9d795bc9fe52d02ea6e0f9a0e1034bd803c8e95
Valti <valentinalondono911@gmail.com>
2024-12-14 14:59:55 -0500

    Adjust on gallery

--- src/utilities/activities.js
+++ src/utilities/activities.js
@@ -1,13 +1,13 @@
 export const images = [
     {
-        img: 'https://firebasestorage.googleapis.com/v0/b/gym-stats-e3823.appspot.com/o/univalleGermany%2FGallery%2F4bbd23650035c3a49c9b887d5c656ecd.jpg?alt=media&token=0cfb63a5-8fc5-4703-91f2-5b624ee68d6d',
-        title: 'Gato gordo',
-        description: 'aaLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore piscing elit'
+        img: 'https://firebasestorage.googleapis.com/v0/b/gym-stats-e3823.appspot.com/o/univalleGermany%2FGallery%2FJORE_Electronica.jpg?alt=media&token=f88a612c-dff4-4cf4-b2fb-b03ee4c2cc50',
+        title: 'XIX Regional Electronics Conference – Robotics Development',
+        description: 'Focused on robotics and industrial development, this annual event highlights innovation in electronics through competitions and academic projects. One of our team members won the robotics competition by creating an advanced robot, showcasing exceptional skills and creativity.'
     },
     {
-        img: 'https://firebasestorage.googleapis.com/v0/b/gym-stats-e3823.appspot.com/o/univalleGermany%2FGallery%2F510cfe8068952a7fb19f884a050a0021.jpg?alt=media&token=24d3f92c-860f-4cca-a47a-83ba6f6137b2',
-        title: 'Yoo',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore piscing elit'
+        img: 'https://firebasestorage.googleapis.com/v0/b/gym-stats-e3823.appspot.com/o/univalleGermany%2FGallery%2FPrimerEncuentroIntercultural.jpg?alt=media&token=756f0d82-c108-4987-ae9c-d8d03c7359e6',
+        title: 'First Intercampus Intercultural Meeting',
+        description: 'We participated in the First Intercampus Intercultural Meeting, where we explored diverse communities and their experiences. This enriching event helped us become more socially aware and deepened our understanding of cultural diversity and inclusion.'
     },
     {
         img: 'https://firebasestorage.googleapis.com/v0/b/gym-stats-e3823.appspot.com/o/univalleGermany%2FGallery%2FBerlin-Capital-Of-Germany-5k-Samsung-Galaxy-Note-9-iphone-8-plus.jpg?alt=media&token=1c5620b5-04cc-4bc1-9a7b-1a483dd77a2a',
@@ -15,23 +15,23 @@ export const images = [
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore piscing elit'
     },
     {
+        img: 'https://firebasestorage.googleapis.com/v0/b/gym-stats-e3823.appspot.com/o/univalleGermany%2FGallery%2FRecaudarFondos.jpg?alt=media&token=b7625499-fa29-4350-8d22-154c10311f4f',
+        title: 'Entrepreneurship Fair Presentation',
+        description: 'At the Universidad del Valle’s Entrepreneurship Fair, we showcased our business initiative aimed at raising funds for our academic trip to Germany. This event allowed us to present our project, connect with the community, and take the first step toward building savings for this important journey.'
+    },
+    {
+        img: 'https://firebasestorage.googleapis.com/v0/b/gym-stats-e3823.appspot.com/o/univalleGermany%2FGallery%2FEnglishClass1.jpg?alt=media&token=d66877a8-51cf-41b8-a0ef-b3e1efabf3ed',
+        title: 'Learning English Together',
+        description: 'In preparation for our academic trip to Germany, we gathered in a classroom to attend elementary English classes. These sessions are a crucial step in improving our language skills and ensuring effective communication during our international experience.'
+    },
+    {
+        img: 'https://firebasestorage.googleapis.com/v0/b/gym-stats-e3823.appspot.com/o/univalleGermany%2FGallery%2FRippioExposicion.jpg?alt=media&token=2dc7f4ce-20c3-4eb8-b06f-a0a5599f77ab',
+        title: 'Engineering Week: AI for Innovative Solutions',
+        description: 'During Universidad del Valle’s Engineering Week, we presented Rippio Food, a web application prototype showcasing how Artificial Intelligence can drive innovative solutions. This event highlighted our skills in web development and problem-solving, reinforcing our commitment to cutting-edge technology.'
+    },
+    {
         img: 'https://firebasestorage.googleapis.com/v0/b/gym-stats-e3823.appspot.com/o/univalleGermany%2FGallery%2Fimage-asset.jpeg?alt=media&token=06b054f3-48a5-4e98-9cd0-54d541593274',
         title: 'Muy bonito',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore piscing elit'
-    },
-    {
-        img: 'https://firebasestorage.googleapis.com/v0/b/gym-stats-e3823.appspot.com/o/univalleGermany%2FGallery%2Fphoto-1467269204594-9661b134dd2b.jpg?alt=media&token=a0db618c-5d87-4939-a3ad-261e485121b2',
-        title: 'Waooooo',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore piscing elit'
-    },
-    {
-        img: 'https://firebasestorage.googleapis.com/v0/b/gym-stats-e3823.appspot.com/o/univalleGermany%2FGallery%2Fberlin-germany-oderberger-straße-aesthetic-thumbnail.jpg?alt=media&token=6cf542e3-9e30-4480-a15b-cc7cdfb18a23',
-        title: 'Azul y lindo',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore piscing elit'
-    },
-    {
-        img: 'https://firebasestorage.googleapis.com/v0/b/gym-stats-e3823.appspot.com/o/univalleGermany%2Fcat.jpg?alt=media&token=c8fa28cf-6fc0-4715-8207-ac6a40f471d8',
-        title: 'Joan Manuel',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore piscing elit'
     },
 ];
--- src/utilities/students.js
+++ src/utilities/students.js
@@ -1,42 +1,42 @@
 export const data = [
     {
         name: 'Valentina Londoño',
-        img: 'https://firebasestorage.googleapis.com/v0/b/gym-stats-e3823.appspot.com/o/univalleGermany%2Fcat.jpg?alt=media&token=c8fa28cf-6fc0-4715-8207-ac6a40f471d8',
+        img: 'https://firebasestorage.googleapis.com/v0/b/gym-stats-e3823.appspot.com/o/univalleGermany%2Fcat.png?alt=media&token=c8fa28cf-6fc0-4715-8207-ac6a40f471d8',
         review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore piscing elit'
     },
     {
         name: 'Juan Cataño',
-        img: 'https://firebasestorage.googleapis.com/v0/b/gym-stats-e3823.appspot.com/o/univalleGermany%2Fcat.jpg?alt=media&token=c8fa28cf-6fc0-4715-8207-ac6a40f471d8',
+        img: 'https://firebasestorage.googleapis.com/v0/b/gym-stats-e3823.appspot.com/o/univalleGermany%2Fcat.png?alt=media&token=c8fa28cf-6fc0-4715-8207-ac6a40f471d8',
         review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore piscing elit'
     },
     {
         name: 'Daniel Marquez',
-        img: 'https://firebasestorage.googleapis.com/v0/b/gym-stats-e3823.appspot.com/o/univalleGermany%2Fcat.jpg?alt=media&token=c8fa28cf-6fc0-4715-8207-ac6a40f471d8',
+        img: 'https://firebasestorage.googleapis.com/v0/b/gym-stats-e3823.appspot.com/o/univalleGermany%2Fcat.png?alt=media&token=c8fa28cf-6fc0-4715-8207-ac6a40f471d8',
         review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore piscing elit'
     },
     {
         name: 'Joan Jaramillo',
-        img: 'https://firebasestorage.googleapis.com/v0/b/gym-stats-e3823.appspot.com/o/univalleGermany%2Fcat.jpg?alt=media&token=c8fa28cf-6fc0-4715-8207-ac6a40f471d8',
+        img: 'https://firebasestorage.googleapis.com/v0/b/gym-stats-e3823.appspot.com/o/univalleGermany%2Fcat.png?alt=media&token=c8fa28cf-6fc0-4715-8207-ac6a40f471d8',
         review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore piscing elit'
     },
     {
         name: 'Jose Luis Ramos',
-        img: 'https://firebasestorage.googleapis.com/v0/b/gym-stats-e3823.appspot.com/o/univalleGermany%2Fcat.jpg?alt=media&token=c8fa28cf-6fc0-4715-8207-ac6a40f471d8',
+        img: 'https://firebasestorage.googleapis.com/v0/b/gym-stats-e3823.appspot.com/o/univalleGermany%2Fcat.png?alt=media&token=c8fa28cf-6fc0-4715-8207-ac6a40f471d8',
         review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore piscing elit'
     },
     {
         name: 'Juan Sebastian Marin',
-        img: 'https://firebasestorage.googleapis.com/v0/b/gym-stats-e3823.appspot.com/o/univalleGermany%2Fcat.jpg?alt=media&token=c8fa28cf-6fc0-4715-8207-ac6a40f471d8',
+        img: 'https://firebasestorage.googleapis.com/v0/b/gym-stats-e3823.appspot.com/o/univalleGermany%2Fcat.png?alt=media&token=c8fa28cf-6fc0-4715-8207-ac6a40f471d8',
         review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore piscing elit'
     },
     {
         name: 'Lina Muñoz',
-        img: 'https://firebasestorage.googleapis.com/v0/b/gym-stats-e3823.appspot.com/o/univalleGermany%2Fcat.jpg?alt=media&token=c8fa28cf-6fc0-4715-8207-ac6a40f471d8',
+        img: 'https://firebasestorage.googleapis.com/v0/b/gym-stats-e3823.appspot.com/o/univalleGermany%2Fcat.png?alt=media&token=c8fa28cf-6fc0-4715-8207-ac6a40f471d8',
         review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore piscing elit'
     },
     {
         name: 'Diego Moreno',
-        img: 'https://firebasestorage.googleapis.com/v0/b/gym-stats-e3823.appspot.com/o/univalleGermany%2Fcat.jpg?alt=media&token=c8fa28cf-6fc0-4715-8207-ac6a40f471d8',
+        img: 'https://firebasestorage.googleapis.com/v0/b/gym-stats-e3823.appspot.com/o/univalleGermany%2Fcat.png?alt=media&token=c8fa28cf-6fc0-4715-8207-ac6a40f471d8',
         review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore piscing elit'
     },
 ];
